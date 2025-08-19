4b3630b785f7445c50f76eb686393fd36202eeb0
Tanh <tuananhzalo@gmail.com>
2025-08-19 11:54:53 +0700

    ...

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -5,13 +5,13 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 // created
 const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
+    {event: 'clerk/user.created'}, 
     async({ event })=>{
-        const {id, frist_name, last_name, email_addresses, image_url} = event.data
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
             _id: id,
-            email: email_addresses[0].email_addresses,
-            name:frist_name + '' + last_name,
+            email: email_addresses[0].email_address,
+            name:first_name + ' ' + last_name,
             image: image_url
         }
         await User.create(userData)
@@ -27,15 +27,15 @@ const syncUserDeletion = inngest.createFunction(
     }
 )
 // updation
-const syncUserUpdation = inngest.createFunction(
+const syncUserUpdation = inngest.createFunction(  
     {id: 'update-user-from-clerk'},
     {event: 'clerk/user.updated'},
     async({ event })=>{
-        const {id, frist_name, last_name, email_addresses, image_url} = event.data
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
             _id: id,
-            email: email_addresses[0].email_addresses,
-            name:frist_name + '' + last_name,
+            email: email_addresses[0].email_address,
+            name:first_name + '' + last_name,
             image: image_url
         }
         await User.findByIdAndUpdate(id,userData)
